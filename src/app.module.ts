19dfb715f5a244b188f1d970ffac8ee1091aeba0
Dean West <dean.west@provenmethod.com>
2021-09-30 15:00:33 -0400

    Getting app to start successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [UserModule, PostModule],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+    UserModule,
+    PostModule,
+  ],
   controllers: [],
   providers: [],
 })
